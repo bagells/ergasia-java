64f58a9ade5180fdcd5162ee0ba48f636ef5577c
Κωνσταντινος Ματτας <109415079+Malakotadin@users.noreply.github.com>
2026-06-07 17:45:04 +0300

    Delete 09c_mvc_example_with_db_login_sessions_jsp_bcrypt.zip_expanded directory
    fuck this guy

--- 09c_mvc_example_with_db_login_sessions_jsp_bcrypt.zip_expanded/09c_mvc-example_with_db_login_session_jsp_bcrypt/build/classes/database_migration.sql
+++
@@ -1,5 +0,0 @@
--- Increase password column size for BCrypt (60 characters)
-ALTER TABLE USERS 
-MODIFY COLUMN password VARCHAR(60) NOT NULL;
--- Remove salt column (if exists)
-ALTER TABLE USERS DROP COLUMN salt;
