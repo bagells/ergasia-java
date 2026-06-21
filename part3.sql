4c8c7833f96e7d68c29faa8da571343fcdec5e0b
bagells <vtiliakos05@gmail.com>
2026-06-21 15:28:35 +0300

    Update part3.sql

--- part3.sql
+++ part3.sql
@@ -1,48 +1,73 @@
-   BEGIN;
-    create table if not exists grades(
-                                         grade_id numeric,
-                                         grade_number numeric,
-                                         primary key (grade_id)
-    );
-    create table if not exists student(
-                                          id numeric,
-                                          name varchar(30),
-                                          surename varchar(30),
-                                          courses numeric,
-                                          grades"numeric",
-                                          password varchar(30),
-                                          department varchar(30),
-                                          primary key (id),
-                                          foreign key (grades) references grades(grade_id)
-    );
-    create table if not exists courses(
-                                          course_id numeric,
-                                          course_name varchar(30),
-                                           s_id numeric,
-                                          primary key (course_id),
-                                         foreign key (s_id) references  student(id)
-    );
+BEGIN;
 
-    create table if not exists proffesor(
-                                            id numeric,
-                                            name varchar(30),
-                                            surename varchar(30),
-                                            courses numeric,
-                                            password varchar(30),
-                                            department varchar(30),
-                                            primary key (id),
-                                            foreign key (courses) REFERENCES courses(course_id)
 
-    );
-    create table if not exists secratary(
-                                            id numeric,
-                                            name varchar(30),
-                                            surename varchar(30),
-                                            courses numeric,
-                                            password varchar(30),
-                                            department varchar(30),
-                                            primary key (id),
-                                            foreign key (courses) REFERENCES courses(course_id)
-    );
+create table if not exists student(
+                                      id numeric,
+                                      username varchar(30),
+                                      name varchar(30),
+                                      surename varchar(30),
+                                      courses numeric,
+                                      grades"numeric",
+                                      password varchar(30),
+                                      department varchar(30),
+                                      primary key (id)
+);
+create table if not exists courses(
+                                      course_id numeric,
+                                      course_name varchar(30),
+                                      primary key (course_id)
+);
+create table if not exists grades
+(
+    s_id         numeric,
+    c_id         numeric,
+    grade        numeric,
+    exetatistiki numeric,
+    foreign key (s_id) references student(id),
+    foreign key (c_id) references courses(id),
+    primary key (s_id,c_id,exetatistiki)
+ );
+create table if not exists dilosi(
+                                     id numeric,
+                                     c_id numeric,
+                                     s_id numeric,
+                                     primary key  (id),
+                                     foreign key (c_id) references  courses(course_id),
+                                     foreign key (s_Id) references  student(id)
+);
 
-    END;
+
+create table if not exists proffesor(
+                                        id numeric,
+                                        name varchar(30),
+                                        username varchar(30),
+                                        surename varchar(30),
+                                        courses numeric,
+                                        password varchar(30),
+                                        department varchar(30),
+                                        primary key (id),
+                                        foreign key (courses) REFERENCES courses(course_id)
+
+);
+create table if not exists dilosi_k(
+                                       id numeric,
+                                       p_id numeric,
+                                       c_id numeric,
+                                       primary key (id),
+                                       foreign key(p_id) references proffesor(id),
+                                       foreign key (c_id) references courses(course_Id)
+);
+create table if not exists secratary(
+                                        id numeric,
+                                        username varchar(30),
+                                        name varchar(30),
+                                        surename varchar(30),
+                                        courses numeric,
+                                        password varchar(30),
+                                        department varchar(30),
+                                        primary key (id),
+                                        foreign key (courses) REFERENCES courses(course_id)
+);
+
+
+END;
